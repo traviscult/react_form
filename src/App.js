369ea0720fc82128c829c516e488539cda454104
traviscult <60018617+traviscult@users.noreply.github.com>
2023-02-08 19:05:13 -0600

    added submit handler on submit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleFirstNameInputChange = (event) => {
+    setValues({ ...values, firstName: event.target.value });
+  };
+
+  const handleLastNameInputChange = (event) => {
+    setValues({ ...values, lastName: event.target.value });
+  };
+
+  const handleemailInputChange = (event) => {
+    setValues({ ...values, email: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+
   return (
     <div className="form-div">
-      <form className="user-form">
+      <form className="user-form" onSubmit={handleSubmit}>
+        {submitted ? (
+          <div className="success-message">
+            Success! Thank you for registering.
+          </div>
+        ) : null}
         <input
+          onChange={handleFirstNameInputChange}
+          value={values.firstName}
           className="form-field"
           type="text"
           placeholder="First Name"
         ></input>
         <input
+          onChange={handleLastNameInputChange}
+          value={values.lastName}
           className="form-field"
           type="text"
           placeholder="Last Name"
         ></input>
-        <input className="form-field" type="text" placeholder="Email"></input>
+        <input
+          onChange={handleemailInputChange}
+          value={values.email}
+          className="form-field"
+          type="text"
+          placeholder="Email"
+        ></input>
         <button className="form-field" type="submit">
           Register
         </button>
